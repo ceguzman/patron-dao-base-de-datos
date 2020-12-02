129ea8846fc2d5170109c54c81d4db4a9b05e7d3
Carlos <ceguzman582@misena.edu.co>
2020-12-02 15:42:21 -0500

    chore: creando el ClienteDTO

---
+++ java/src/main/java/co/edu/carlos/modelo/dto/ClienteDTO.java
@@ -0,0 +1,46 @@
+package co.edu.carlos.modelo.dto;
+
+import java.io.Serializable;
+
+public class ClienteDTO implements Serializable {
+
+    private static long serialVersionUID = 48L;
+
+    private String sigla;
+    private String numeroDocumento;
+    private String nombres;
+    private String apellidos;
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+}
