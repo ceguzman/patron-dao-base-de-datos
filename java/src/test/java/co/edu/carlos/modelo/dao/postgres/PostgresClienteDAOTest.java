a2bafd864997057081e56de5210a5bbdcaeeb4e4
Carlos <ceguzman582@misena.edu.co>
2020-12-02 17:36:25 -0500

    chore: PostgresClienteDao Test a todos los metos

--- java/src/test/java/co/edu/carlos/modelo/dao/postgres/PostgresClienteDAOTest.java
+++ java/src/test/java/co/edu/carlos/modelo/dao/postgres/PostgresClienteDAOTest.java
@@ -6,16 +6,17 @@ import co.edu.carlos.modelo.factory.ClienteDAOFactory;
 import co.edu.carlos.modelo.factory.postgres.PostgresClienteDAOFactory;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class PostgresClienteDAOTest {
+
 
     @Test
     void insert() {
         ClienteDTO clienteDTO = new ClienteDTO();
         clienteDTO.setSigla("CE");
-        clienteDTO.setNumeroDocumento("1001061274");
+        clienteDTO.setNumeroDocumento("1001201789");
         clienteDTO.setNombres("Carlos Esteban");
         clienteDTO.setApellidos("Guzman Baquero");
 
@@ -26,17 +27,37 @@ class PostgresClienteDAOTest {
 
     @Test
     void update() {
+        ClienteDTO clienteDTO = new ClienteDTO();
+        clienteDTO.setSigla("CE");
+        clienteDTO.setNumeroDocumento("1001201789");
+        clienteDTO.setNombres("Jessica");
+        clienteDTO.setApellidos("Granados Hernandez");
+
+        ClienteDAOFactory fabrica = new PostgresClienteDAOFactory();
+        ClienteDAO clienteDAO = fabrica.create();
+        assertEquals(clienteDAO.update(clienteDTO, "CE", "1001201789"), 1);
     }
 
     @Test
     void delete() {
+        ClienteDTO clienteDTO = new ClienteDTO();
+
+        ClienteDAOFactory fabrica = new PostgresClienteDAOFactory();
+        ClienteDAO clienteDAO = fabrica.create();
+        assertEquals(clienteDAO.delete("CE", "1001061274"), 1);
     }
 
     @Test
     void findAll() {
+        ClienteDAOFactory fabrica = new PostgresClienteDAOFactory();
+        ClienteDAO clienteDAO = fabrica.create();
+        assertFalse(clienteDAO.findAll().isEmpty());
     }
 
     @Test
     void findPk() {
+        ClienteDAOFactory fabrica = new PostgresClienteDAOFactory();
+        ClienteDAO clienteDAO = fabrica.create();
+        assertNotEquals(clienteDAO.findPk("CE","1001201789"), null);
     }
 }
